cf146bad011170aad4d399f3c4903c248e60390a
admin <admin@simples.in>
2017-12-18 11:39:51 +0530

    facebook login

--- app/src/main/java/universal/universalthought/SplashScreen.java
+++ app/src/main/java/universal/universalthought/SplashScreen.java
@@ -3,12 +3,20 @@ package universal.universalthought;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import universal.universalthought.activity.MainActivity;
 
@@ -18,12 +26,29 @@ public class SplashScreen extends AppCompatActivity {
     private PrefManager prefManager;
     public static final String mypreference = "mypref";
     private static int SPLASH_TIME_OUT = 3000;
-
+    PackageInfo info;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try {
+            info = getPackageManager().getPackageInfo("simplicity_an.simplicity_an", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
         setContentView(R.layout.splashscreen);
         sharedpreferences =  getSharedPreferences(mypreference,
                 Context.MODE_PRIVATE);
